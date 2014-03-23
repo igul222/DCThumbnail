062bc7603bf7eafa69b19380cae09a6bfd80139c
Ishaan Gulrajani <igul222@gmail.com>
2014-03-22 21:26:29 -0700

    Initial Commit

---
+++ DCThumbnail/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DCThumbnail
+//
+//  Created by Ishaan Gulrajani on 3/22/14.
+//  Copyright (c) 2014 DeskConnnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DCAppDelegate class]));
+    }
+}
